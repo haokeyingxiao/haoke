094c05d5e9e97fc55c7e611d36e8d24d433e5f1b
gxiaosong <lyricsong47@gmail.com>
2024-05-31 15:02:22 +0800

    fix create tag

---
+++ src/Core/Haoke/Haoke.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Shopware\Core\Haoke;
+
+use Shopware\Core\Framework\Bundle;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Kernel;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\ClosureLoader;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\IniFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * @internal
+ */
+#[Package('core')]
+class Haoke extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection'));
+        $loader->load('services.xml');
+        /** @var string $environment */
+        $environment = $container->getParameter('kernel.environment');
+        $this->buildConfig($container, $environment);
+    }
+    private function buildConfig(ContainerBuilder $container, string $environment): void
+    {
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new XmlFileLoader($container, $locator),
+            new YamlFileLoader($container, $locator),
+            new IniFileLoader($container, $locator),
+            new PhpFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+            new ClosureLoader($container),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = $this->getPath() . '/Resources/config';
+
+        $configLoader->load($confDir . '/{packages}/*' . Kernel::CONFIG_EXTS, 'glob');
+        $configLoader->load($confDir . '/{packages}/' . $environment . '/*' . Kernel::CONFIG_EXTS, 'glob');
+    }
+}
