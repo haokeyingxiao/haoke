508620766797f81ca931a5c319f51cfcc2c9c6a5
gxiaosong <lyricsong47@gmail.com>
2024-06-13 23:44:20 +0800

    Change default currency

--- tests/migration/Core/BasicDataUntouchedTest.php
+++ tests/migration/Core/BasicDataUntouchedTest.php
@@ -19,7 +19,7 @@ class BasicDataUntouchedTest extends TestCase
         static::assertIsString($file);
 
         static::assertSame(
-            'ca7588953a57a2bd9aae0401dafc1bb1577d510d',
+            '293eac005bf5bb755cb64a9e144b90c94f3f1a46',
             sha1_file($file),
             'BasicData migration has changed. This is not allowed.'
         );
